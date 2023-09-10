5c692c478c3d92ab9e16f8142ddda73e7c2cdb1e
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-09-10 18:57:06 +0100

    feat: next js api for auth

---
+++ reelhub/frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,37 @@
+
+import NextAuth from "next-auth"
+import type { AuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions: AuthOptions = {
+    providers: [
+      CredentialsProvider({
+        name: 'Credentials',
+        credentials: {
+            email: {
+                label: "Email",
+                type: "email",
+                placeholder: "example@example.com",
+              },
+              password: { label: "Password", type: "password" },
+        },
+        async authorize (credentials, req) {
+          if (typeof credentials !== "undefined") {
+            const res = await authenticate(credentials.email, credentials.password)
+            if (typeof res !== "undefined") {
+              return { ...res.user, apiToken: res.token }
+            } else {
+              return null
+            }
+          } else {
+            return null
+          }
+        }
+      })
+    ],
+    session: { strategy: "jwt" }
+  }
+  
+  const handler = NextAuth(authOptions)
+  
+  export { handler as GET, handler as POST }
