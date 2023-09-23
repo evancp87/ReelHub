e5042aa8ff047940882513e14cadc88f882cce5b
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-09-23 23:34:26 +0100

    feat: updated styles and added active links

--- reelhub/frontend/src/components/Sidebar.tsx
+++ reelhub/frontend/src/components/Sidebar.tsx
@@ -21,17 +21,23 @@ import { AppDispatch, RootState } from "@/store/store";
 import { useDispatch, useSelector } from "react-redux";
 import { useLogoutUserMutation } from "../store/services/userApi";
 // import { useGetUserInfoQuery } from "../store/services/userApi";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 import { selectCurrentUser, logout } from "../store/services/usersSlice";
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
 type Props = {};
 
 export default function Sidebar({}: Props) {
   const user = useAppSelector(selectCurrentUser);
   const router = useRouter();
+  // const isActive = (href) => router.isReady && router.pathname.startsWith(href);
+  const currentRoute = usePathname();
+  // const isActive = (href) =>
+  //   router.pathname ? router.pathname.startsWith(href) : false;
+
+  // console.log(isActive);
+
   console.log("the user is", user);
   console.log("checking the id", user?._id);
   const dispatch = useAppDispatch();
@@ -60,38 +66,85 @@ export default function Sidebar({}: Props) {
   // console.log("user data", data?.firstName);
   return (
     <>
-      <aside className=" flex h-24 flex-row items-center justify-between bg-darkBlue sm:mx-[0.5em] sm:mt-[1.5em] sm:rounded-xl md:mx-[2em] md:h-[70vh] md:w-[200px] md:max-w-[200px] md:flex-col">
+      <aside className=" flex h-24 flex-row items-center justify-between bg-darkBlue sm:mx-[0.5em] sm:mt-[1.5em] sm:rounded-xl md:mx-[2em] md:h-[70vh] md:w-[75px] md:max-w-[75x] md:flex-col lg:h-screen">
         <Image
-          className=" ml-4 md:ml-0 md:mt-4"
+          className=" ml-4 mr-2 sm:mr-0 md:ml-0 md:mt-4"
           src={Logo}
           alt="logo"
           width="32"
           height="25"
         />
         <div className="flex flex-row gap-4 md:flex-col">
-          <Link href="/dashboard/movies">
-            <Image src={NavMovies} alt="logo" width="32" height="25" />
-          </Link>
           <Link href="/dashboard">
-            <Image src={NavHome} alt="logo" width="32" height="25" />
+            <Image
+              className={`nav-icon ${
+                currentRoute === "/dashboard" ? "active" : ""
+              }`}
+              src={NavHome}
+              alt="dashboard home link"
+              width="32"
+              height="25"
+            />
           </Link>
-          <Link href="/dashboard/bookmarks">
-            <Image src={NavBookmark} alt="logo" width="32" height="25" />
+          <Link href="/dashboard/movies">
+            <Image
+              className={`nav-icon ${
+                currentRoute === "/dashboard/movies" ? "active" : ""
+              }`}
+              src={NavMovies}
+              alt="movies link"
+              width="32"
+              height="25"
+            />
           </Link>
           <Link href="/dashboard/tv">
-            <Image src={NavTv} alt="logo" width="32" height="25" />
+            <Image
+              className={`nav-icon ${
+                currentRoute === "/dashboard/tv" ? "active" : ""
+              }`}
+              src={NavTv}
+              alt="tv link"
+              width="32"
+              height="25"
+            />
+          </Link>
+          <Link href="/dashboard/bookmarks">
+            <Image
+              className={`nav-icon ${
+                currentRoute === "/dashboard/bookmarks" ? "active" : ""
+              }`}
+              src={NavBookmark}
+              alt="bookmarks link"
+              width="32"
+              height="25"
+            />
           </Link>
         </div>
         {/* TODO: conditionally render */}
-        <Image
-          className="mr-4 md:mb-4 md:mr-0"
-          src={Avatar}
-          alt="logo"
-          width="32"
-          height="25"
-        />
-        {user && <p>Welcome, {user?.firstName}</p>}
-        <button onClick={handleLogout}> {user ? "Logout" : ""} </button>
+
+        <div className=" mr-2 mt-2 flex flex-wrap justify-center sm:mt-0 sm:mt-0  md:flex-col md:items-center">
+          <div className="flex flex-wrap justify-end sm:items-center sm:justify-center md:flex-col ">
+            <Image
+              className="mr-4 md:mb-4 md:mr-0"
+              src={Avatar}
+              alt="logo"
+              width="32"
+              height="25"
+            />
+            {user && (
+              <p className="mb-4 hidden p-2 text-xs sm:block">
+                Welcome, {user?.firstName}
+              </p>
+            )}
+            <button
+              className="mb-4 mt-2 rounded-lg bg-red p-2 text-xs hover:bg-white hover:text-black sm:mt-0"
+              onClick={handleLogout}
+            >
+              {" "}
+              {user ? "Logout" : ""}{" "}
+            </button>
+          </div>
+        </div>
       </aside>
     </>
   );
