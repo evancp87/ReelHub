5cfe31232aa1fe0200da47aa0ba1770cfbcc7673
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-09-29 21:23:02 +0100

    chore: code tidy

--- reelhub/frontend/src/store/services/userApi.ts
+++ reelhub/frontend/src/store/services/userApi.ts
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import {AuthToken, RegisterCredentials, LoginCredentials, User } from "./types";
-import {login, registerUser, logout} from "./usersSlice";
 import {RootState} from "../store";
 
   const BASE_API_URL = "http://localhost:6002";
@@ -29,30 +28,16 @@ import {RootState} from "../store";
         }),
       }),
       loginUser: builder.mutation<AuthToken, LoginCredentials>
-      // ({
-      //   query: (data) => ({
-      //     url: '/users/login',
-      //     method: 'POST',
-      //     body: data,
-      //     credentials: 'include',
-      //   }),
-      // }),
+     
       ({
         query(data: { email: string, password: string }) {
           return {
             url: '/users/login',
             method: 'POST',
             body: data,
-            // credentials: 'include',
           };
         },
-        // async onQueryStarted(args, { dispatch, queryFulfilled }) {
-        //   try {
-        //     await queryFulfilled;
-        //     const email = args.email;
-        //     await dispatch(userApi.endpoints.getUserInfo.initiate(null));
-        //   } catch (error) {}
-        // },
+    
       }),
       logoutUser: builder.mutation<void, void>({
         query() {
@@ -62,48 +47,8 @@ import {RootState} from "../store";
           };
         },
       }),
-      // getUserInfo: builder.query<User, null>({
-      //   query() {
-      //     return {
-      //       url: `/users/user/`,
-      //       // credentials: 'include',
-      //       // headers: {
-      //       //   Authorization: `Bearer ${token}`
-      //       // }
-      //     };
-      //   },
-        // transformResponse: (result: { data: { user: User } }) =>
-        //   result.data.user,
-        // async onQueryStarted(args, { dispatch, queryFulfilled }) {
-        //   try {
-        //     const { data } = await queryFulfilled;
-        //     dispatch(registerUser(data));
-        //   } catch (error) {}
-        // },
-      // }),
     }),
   });
 
 
-  // const baseQueryWithReAuth = async (args, api, extraOptions) => {
-  //   let result = await baseQuery(args, api, extraOptions)
-  //   if (result?.error?.originalStatus === 403) {
-  //     console.log("sending refresh token")
-  //     // send refresh token to get new access token
-  //     const refreshResult = await baseQuery("/refresh", api, extraOptions)
-  //   console.log(refreshResult);
-  //   if (refreshResult?.data) {
-  //     const user = api.getState().auth.user;
-  //     api.dispatch(setCredentials({...refreshResult.data, user}))
-
-  //     result = await baseQueryWithReAuth(arg, api )
-  //   }
-    
-  //   }
-  // }
-
-  export const { useCreateUserMutation, useLoginUserMutation, useLogoutUserMutation,
-    
-    // useGetUserInfoQuery 
-  } = userApi;
-
+  export const { useCreateUserMutation, useLoginUserMutation, useLogoutUserMutation } = userApi;
