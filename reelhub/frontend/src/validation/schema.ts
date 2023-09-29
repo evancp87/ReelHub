71239911bb5f28e52b48983b2976fc30c53b4ae3
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-09-29 21:23:50 +0100

    feat: update to joi schemas

--- reelhub/frontend/src/validation/schema.ts
+++ reelhub/frontend/src/validation/schema.ts
@@ -1,27 +1,57 @@
-import joi from "joi"
-
+import joi from "joi";
 
 export const loginSchema = {
-    email:
-    joi.string(),
-    password: joi.string().trim()
-    .min(1).message("Can't be empty")
-    .max(40).message("Too long")
-}
+  email: joi.string().messages({
+    'string.email': "Invalid email format",
+    'string.empty': "Can't be empty",
+  }),
+  password: joi.string().trim().messages({
+    'string.empty': "Can't be empty"
+  })
+};
 
 export const registerSchema = {
-    firstName: joi.string().min(1).message("Can't be empty").max(50).message("Too long"),
-    lastName: joi.string().min(1).message("Too short").max(50).message("Too long"),
-    email:
-    joi.string().email({ minDomainSegments: 2, tlds: { allow: false } }).messages({
-        "string.pattern.base": "can't be empty",
-      }),
-    password: joi.string().trim()
-    .min(5)
-    .max(40)
-    .regex(/[0-9a-zA-Z]*\d[0-9a-zA-Z]*/)
-    .messages({
-      "object.regex": "Must have one number",
-      "string.pattern.base": "At least one number required",
-    })
-}
+  firstName: joi.string().min(1).max(50).messages({
+    'string.min': "Must be 1 or more characters",
+    'string.max': "Must be 50 characters or less",
+    'string.empty': "Can't be empty",
+
+  }),
+  lastName: joi.string().min(1).max(50).messages({
+    'string.min': "Must be 1 or more characters",
+    'string.max': "Must be 50 characters or less",
+    'string.empty': "Can't be empty",
+
+  }),
+  email: joi.string().email({ minDomainSegments: 2, tlds: { allow: false } }).messages({
+    'string.email': "Invalid email format",
+    'string.empty': "Can't be empty",
+  }),
+  password: joi.string().trim()
+  .min(5).message("Must be 5 or more characters")
+  .max(40).message("Must be 40 characters or less")
+  .regex(/[0-9a-zA-Z]*\d[0-9a-zA-Z]*/).message("Must have at least one number").messages({'string.empty': "Can't be empty"}),
+  repeatPassword: joi.string().trim().valid(joi.ref('password')).messages({
+    'any.only': 'Passwords must match',
+    'string.empty': "Can't be empty",
+  }),
+  // .custom((value, helpers) => {
+  //   if (value === helpers.state.password) {
+  //     return value; // Passwords match
+  //   } else {
+  //     return helpers.message('Passwords must match');
+  //   }
+  // })
+  // .messages({
+  //   'string.empty': "Can't be empty",
+  // }),
+  // .valid(joi.ref('password'))
+  // .options({
+  //   language: {
+  //     any: {
+  //       allowOnly: '!!Passwords do not match',
+  //     }
+  //   } 
+  // })
+ 
+};
