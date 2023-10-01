575448aa3cb0c63210abf8f67312e126f555df76
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-10-01 15:52:49 +0100

    feat: prevent auth users visiting certain pages

--- reelhub/frontend/src/components/AuthProvider.tsx
+++ reelhub/frontend/src/components/AuthProvider.tsx
@@ -2,21 +2,36 @@
 import { useRouter } from "next/navigation";
 import { useIsAuthenticated } from "../store/hooks";
 import React, { useEffect } from "react";
-
+import { usePathname } from "next/navigation";
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const isAuthenticated = useIsAuthenticated();
+
   const router = useRouter();
+  const currentRoute = usePathname();
+
+  const currUser =
+    typeof localStorage !== "undefined"
+      ? JSON.parse(localStorage.getItem("user"))
+      : null;
 
   useEffect(() => {
     if (!isAuthenticated) {
       router.push("/authentication/login");
     }
-  }, [isAuthenticated, router]);
+
+    if (
+      currUser &&
+      (currentRoute === "/authentication/login" ||
+        currentRoute === "/authentication/register" ||
+        currentRoute === "/")
+    ) {
+      router.push("/dashboard");
+    }
+  }, [isAuthenticated, router, currUser, currentRoute]);
 
   if (!isAuthenticated) {
     return null;
   }
-
   return children;
 };
 
