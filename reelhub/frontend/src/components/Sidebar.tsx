5e3d3ae8ec8cf45637fb67a70bafe6600e18b26d
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-10-04 14:55:16 +0100

    fix: fixed hydration issue

--- reelhub/frontend/src/components/Sidebar.tsx
+++ reelhub/frontend/src/components/Sidebar.tsx
@@ -1,11 +1,6 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import Logo from "../../public/assets/logo.svg";
-import NavBookmark from "../../public/assets/icon-nav-bookmark.svg";
-import NavHome from "../../public/assets/icon-nav-home.svg";
-import NavTv from "../../public/assets/icon-nav-tv-series.svg";
-import NavMovies from "../../public/assets/icon-nav-movies.svg";
 import Link from "next/link";
 import { useAppDispatch, useAppSelector } from "@/utils/helpers";
 import { useRouter, usePathname } from "next/navigation";
@@ -24,13 +19,20 @@ export default function Sidebar() {
     dispatch(logout());
     router.push("/authentication/login");
   };
+  const [isMounted, setIsMounted] = useState(false);
 
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
   return (
     <>
-      <aside className=" flex h-24 flex-row items-center justify-between bg-darkBlue sm:mx-[0.5em] sm:mt-[1.5em] sm:rounded-xl md:mx-[2em] md:h-[70vh] md:w-[75px] md:max-w-[75x] md:flex-col lg:h-screen">
+      <aside
+        suppressHydrationWarning={true}
+        className=" flex h-24 flex-row items-center justify-between bg-darkBlue sm:mx-[0.5em] sm:mt-[1.5em] sm:rounded-xl md:mx-[2em] md:h-[70vh] md:w-[75px] md:max-w-[75x] md:flex-col lg:h-screen"
+      >
         <Image
           className=" ml-4 mr-2 sm:mr-0 md:ml-0 md:mt-4"
-          src={Logo}
+          src="https://reelhub.s3.eu-west-2.amazonaws.com/logo.svg"
           alt="logo"
           width="32"
           height="25"
@@ -41,7 +43,7 @@ export default function Sidebar() {
               className={`nav-icon ${
                 currentRoute === "/dashboard" ? "active" : ""
               }`}
-              src={NavHome}
+              src="https://reelhub.s3.eu-west-2.amazonaws.com/icon-nav-home.svg"
               alt="dashboard home link"
               width="32"
               height="25"
@@ -52,7 +54,7 @@ export default function Sidebar() {
               className={`nav-icon ${
                 currentRoute === "/dashboard/movies" ? "active" : ""
               }`}
-              src={NavMovies}
+              src="https://reelhub.s3.eu-west-2.amazonaws.com/icon-nav-movies.svg"
               alt="movies link"
               width="32"
               height="25"
@@ -63,7 +65,7 @@ export default function Sidebar() {
               className={`nav-icon ${
                 currentRoute === "/dashboard/tv" ? "active" : ""
               }`}
-              src={NavTv}
+              src="https://reelhub.s3.eu-west-2.amazonaws.com/icon-nav-tv-series.svg"
               alt="tv link"
               width="32"
               height="25"
@@ -74,7 +76,7 @@ export default function Sidebar() {
               className={`nav-icon ${
                 currentRoute === "/dashboard/bookmarks" ? "active" : ""
               }`}
-              src={NavBookmark}
+              src="https://reelhub.s3.eu-west-2.amazonaws.com/icon-nav-bookmark.svg"
               alt="bookmarks link"
               width="32"
               height="25"
@@ -82,15 +84,22 @@ export default function Sidebar() {
           </Link>
         </div>
 
-        <div className=" mr-2 mt-2 flex flex-wrap justify-center sm:mt-0 sm:mt-0  md:flex-col md:items-center">
-          <div className="flex flex-wrap justify-end sm:items-center sm:justify-center md:flex-col ">
-            {user?.avatar ? (
+        <div
+          suppressHydrationWarning={true}
+          className=" mr-2 mt-2 flex flex-wrap justify-center sm:mt-0 sm:mt-0  md:flex-col md:items-center"
+        >
+          <div
+            suppressHydrationWarning={true}
+            className="flex flex-wrap justify-end sm:items-center sm:justify-center md:flex-col "
+          >
+            {isMounted && user?.avatar ? (
               <Image
                 className="mr-4 mt-2 h-[30px] w-[30px] rounded-full md:mb-4 md:mr-0"
                 src={user?.avatar}
                 alt="logo"
                 width="32"
                 height="25"
+                suppressHydrationWarning={true}
               />
             ) : (
               <FontAwesomeIcon
@@ -101,17 +110,19 @@ export default function Sidebar() {
               />
             )}
 
-            {user && (
+            {isMounted && user ? (
               <p className="mb-4 hidden p-2 text-xs sm:block">
                 Welcome, {user?.firstName}
               </p>
+            ) : (
+              ""
             )}
             <button
               className="mb-4 mt-2 rounded-lg bg-red p-2 text-xs hover:bg-white hover:text-black sm:mt-0"
               onClick={handleLogout}
             >
               {" "}
-              {user ? "Logout" : ""}{" "}
+              {isMounted && user ? "Logout" : ""}{" "}
             </button>
           </div>
         </div>
