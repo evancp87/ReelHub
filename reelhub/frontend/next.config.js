63634734da8151ae44790bcab14c535cc64b6ff7
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-10-08 22:58:23 +0100

    feat: tweak to eslint and tsconfig

--- reelhub/frontend/next.config.js
+++ reelhub/frontend/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["reelhub.s3.eu-west-2.amazonaws.com", "res.cloudinary.com"],
   },
